9a962409e1c0b22559150b87de985e47edfd1b5e
Henry Been <henry.been@ing.com>
2019-11-03 17:42:48 +0100

    Add DI and load some secrets

---
+++ SimpleFunction/SimpleFunction.Function/Secret.cs
@@ -0,0 +1,32 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace SimpleFunction.Function
+{
+    public class Secret
+    {
+        private readonly IConfiguration _configuration;
+
+        public Secret(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [FunctionName(nameof(Secret))]
+        public IActionResult Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            var secret = _configuration["Secret"];
+
+            return new OkObjectResult($"Here have a secret: {secret}");
+        }
+    }
+}
---
+++ SimpleFunction/SimpleFunction.Function/StartUp.cs
@@ -0,0 +1,31 @@
+﻿using System.IO;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using SimpleFunction.Function;
+
+[assembly: FunctionsStartup(typeof(Startup))]
+namespace SimpleFunction.Function
+{
+
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            var services = builder.Services;
+
+            var hostingEnvironment = services
+                .BuildServiceProvider()
+                .GetService<IHostingEnvironment>();
+
+            var configurationBuilder = new ConfigurationBuilder()
+                .AddEnvironmentVariables()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("secret.settings.json");
+ 
+            var newConfiguration = configurationBuilder.Build();
+            services.AddSingleton<IConfiguration>(newConfiguration);
+        }
+    }
+}

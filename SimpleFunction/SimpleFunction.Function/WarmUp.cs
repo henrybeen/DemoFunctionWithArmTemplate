a5b3f2e998426b03b8a7f5911bd11dc5ee272a22
Henry Been <henry.been@ing.com>
2019-11-04 20:48:54 +0100

    Adding warm-up settings

---
+++ SimpleFunction/SimpleFunction.Function/WarmUp.cs
@@ -0,0 +1,32 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace SimpleFunction.Function
+{
+    public class WarmUp
+    {
+        private readonly IConfiguration _configuration;
+
+        public WarmUp(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [FunctionName(nameof(WarmUp))]
+        public async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogCritical("Warming up, right now!!");
+
+            await Task.Delay(60000);
+
+            return new OkObjectResult("OK");
+        }
+    }
+}
